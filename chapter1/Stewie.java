9d7903c1bc1491425be58bc0953b1d4fd0b06a50
Alfredo Miranda <alfremir@gmail.com>
2014-01-05 22:14:51 -0600

    Completed Stewie

---
+++ chapter1/Stewie.java
@@ -0,0 +1,13 @@
+/* Write a complete Java program in a class named Stewie that prints the 
+ * following output:
+ * //////////////////////
+ * || Victory is mine! ||
+ * \\\\\\\\\\\\\\\\\\\\\\
+ */
+public class Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
