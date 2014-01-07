fdc8ac1a5da1f5a7d1268a8cf0ce659194fb4bdb
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 22:01:21 -0600

    Completed Egg

---
+++ chapter1/Egg.java
@@ -0,0 +1,19 @@
+/* Write a complete Java program in a class named Egg that displays the 
+ * following output:
+ *   _______
+ *  /       \
+ * /         \
+ * -"-'-"-'-"-
+ * \         /
+ *  \_______/
+ */
+public class Egg {
+    public static void main(String[] args) {
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("-\"-'-\"-'-\"-");
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+    }
+}
