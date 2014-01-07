971a24bcdb8f2290d9c7d6c51346fb4321816d8f
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 21:37:35 -0600

    Completed Mantra

---
+++ chapter1/Mantra.java
@@ -0,0 +1,20 @@
+/* Write a complete Java program in a class named Mantra that produces the 
+ * following output. Remove its redundancy by adding a method.
+ * There's one thing every coder must understand:
+ * The System.out.println command.
+ *
+ * There's one thing every coder must understand:
+ * The System.out.println command.
+ */
+public class Mantra {
+    public static void main(String[] args) {
+        printParagraph();
+        System.out.println();
+        printParagraph();
+    }
+                                    
+    public static void printParagraph() {
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+    }
+}
