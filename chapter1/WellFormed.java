329ea3f5f09914136faee29eb7c7c6d4614db900
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 20:52:25 -0600

    Completed WellFormed

---
+++ chapter1/WellFormed.java
@@ -0,0 +1,24 @@
+/* Write a complete Java program in a class named WellFormed that prints the 
+ * following output:
+ * A well-formed Java program has
+ * a main method with { and }
+ * braces.
+ *
+ * A System.out.println statement
+ * has ( and ) and usually a
+ * String that starts and ends
+ * with a " character.
+ * (But we type \" instead!)
+ */
+public class WellFormed {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has");
+        System.out.println("a main method with { and }");
+        System.out.println("braces.\n");
+        System.out.println("A System.out.println statement");
+        System.out.println("has ( and ) and usually a");
+        System.out.println("String that starts and ends");
+        System.out.println("with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+    }
+}
