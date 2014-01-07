62c8ebddcc4993e049aa79e0e923c341b26a9516
Alfredo Miranda <alfremir@gmail.com>
2014-01-07 11:51:39 -0600

    Completed TwoRockets

---
+++ chapter1/TwoRockets.java
@@ -0,0 +1,47 @@
+/* Write a complete Java program in a class named TwoRockets that generates 
+ * the following output. Use static methods to show structure and eliminate 
+ * redundancy in your solution.
+ *    /\       /\
+ *   /  \     /  \
+ *  /    \   /    \
+ * +------+ +------+
+ * |      | |      |
+ * |      | |      |
+ * +------+ +------+
+ * |United| |United|
+ * |States| |States|
+ * +------+ +------+
+ * |      | |      |
+ * |      | |      |
+ * +------+ +------+
+ *    /\       /\
+ *   /  \     /  \
+ *  /    \   /    \
+ */
+public class TwoRockets {
+    public static void main(String[] args) {
+        printTriangle();
+        printSquare();
+        printUSA();
+        printSquare();
+        printTriangle();
+    }
+                                                    
+    public static void printTriangle() {
+        System.out.println("   /\\       /\\");
+        System.out.println("  /  \\     /  \\");
+        System.out.println(" /    \\   /    \\");
+    }
+                                                                                        
+    public static void printSquare() {
+        System.out.println("+------+ +------+");
+        System.out.println("|      | |      |");
+        System.out.println("|      | |      |");
+        System.out.println("+------+ +------+");
+    }
+                                                                                                                                    
+    public static void printUSA() {
+        System.out.println("|United| |United|");
+        System.out.println("|States| |States|");
+    }
+}
