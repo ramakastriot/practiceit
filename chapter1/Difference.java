0c8f068ba1f44f05e08f4688162ff9f26bce0790
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 21:09:19 -0600

    Completed Difference

---
+++ chapter1/Difference.java
@@ -0,0 +1,16 @@
+/* Write a complete Java program in a class named Difference that prints the 
+ * following output:
+ * What is the difference between
+ * a ' and a "?  Or between a " and a \"?
+ *
+ * One is what we see when we're typing our program.
+ * The other is what appears on the "console."
+ */
+public class Difference {
+    public static void main(String[] args) {
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?\n");
+        System.out.println("One is what we see when we're typing our program.");
+        System.out.println("The other is what appears on the \"console.\"");
+    }
+}
