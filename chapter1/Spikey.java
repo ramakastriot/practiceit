515a0435eefee4a34e933a0083c725b1543dba1d
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 20:40:10 -0600

    Forgot to add the problem description

--- chapter1/Spikey.java
+++ chapter1/Spikey.java
@@ -1,3 +1,12 @@
+/* Write a complete Java program in a class named Spikey that prints the 
+ * following output:
+ *   \/
+ *  \\//
+ * \\\///
+ * ///\\\
+ *  //\\
+ *   /\
+ */
 public class Spikey {
     public static void main(String[] args) {
         System.out.println("  \\/");
