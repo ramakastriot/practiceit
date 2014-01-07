22e1c465d702da36ad89783ba61851b7770806b1
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 20:33:39 -0600

    Completed Spikey

---
+++ chapter1/Spikey.java
@@ -0,0 +1,10 @@
+public class Spikey {
+    public static void main(String[] args) {
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.print("  /\\");
+    }
+}
