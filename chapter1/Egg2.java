7c255e514ed60a9057b53b61f95b30e8000fbbb0
Alfredo Miranda <alfremir@gmail.com>
2014-01-06 22:34:40 -0600

    Completed Egg2

---
+++ chapter1/Egg2.java
@@ -0,0 +1,53 @@
+/* Write a complete Java program in a class named Egg2 that generates the 
+ * following output. Use static methods to show structure and eliminate 
+ * redundancy in your solution.
+ *   _______
+ *  /       \
+ * /         \
+ * \         /
+ *  \_______/
+ * -"-'-"-'-"-
+ *   _______
+ *  /       \
+ * /         \
+ * \         /
+ *  \_______/
+ * -"-'-"-'-"-
+ * \         /
+ *  \_______/
+ *   _______
+ *  /       \
+ * /         \
+ * -"-'-"-'-"-
+ * \         /
+ *  \_______/
+ */
+public class Egg2 {
+    public static void main(String[] args) {
+        printTop();
+        printBottom();
+        printMiddle();
+        printTop();
+        printBottom();
+        printMiddle();
+        printBottom();
+        printTop();
+        printMiddle();
+        printBottom();
+    }
+    
+    public static void printTop() {
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+    }
+                                                                                                                                
+    public static void printMiddle() {
+        System.out.println("-\"-'-\"-'-\"-");
+    }
+                                                                                                                                                    
+    public static void printBottom() {
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+    }
+}
