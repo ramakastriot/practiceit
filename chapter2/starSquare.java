8de13f8e71ce7b8e92ff53c38af57816d1bd14dc
Alfredo Miranda <alfremir@gmail.com>
2014-01-07 17:45:20 -0600

    Completed starSquare

---
+++ chapter2/starSquare.java
@@ -0,0 +1,12 @@
+/* Write for loops to produce the following output:
+ *
+ * *****
+ * *****
+ * *****
+ * *****
+ */
+for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 5; j++)
+        System.out.print("*");
+    System.out.println();
+}
