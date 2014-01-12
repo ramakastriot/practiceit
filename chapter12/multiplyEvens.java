bf5ceeddbc5b3f382271ee4fc9590af1acb546ec
Alfredo Miranda <alfremir@gmail.com>
2014-01-11 19:40:25 -0600

    Completed multiplyEvens

--- chapter12/multiplyEvens.java
+++ chapter12/multiplyEvens.java
@@ -1,3 +1,7 @@
+/* Write a method multiplyEvens that returns the product of the first n even 
+ * integers. You should throw an IllegalArgumentException if passed a value 
+ * less than or equal to 0.
+ */
 public int multiplyEvens(int n) {
     if(n <= 0)
         throw new IllegalArgumentException();
