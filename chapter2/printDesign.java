861eda4c89ca1ec1db6549db011b0a42ef5c769f
Alfredo Miranda <alfremir@gmail.com>
2014-01-14 17:37:23 -0600

    Fixed formatting

--- chapter2/printDesign.java
+++ chapter2/printDesign.java
@@ -7,7 +7,7 @@
  * --7777777--
  * -999999999-
  */
-public static void printDesign() {
+public static void printDesign() {
     for(int i = 1; i <= 9; i+=2) {
         for(int j = 0; j < (11 - i) / 2; j++)
             System.out.print("-");
