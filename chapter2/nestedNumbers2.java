c9e395a4dba6022dccb3971d5b3ea82a0c6a8bdf
Alfredo Miranda <alfremir@gmail.com>
2014-01-14 17:15:48 -0600

    Fixed formatting

--- chapter2/nestedNumbers2.java
+++ chapter2/nestedNumbers2.java
@@ -6,10 +6,10 @@
  * 99999888887777766666555554444433333222221111100000
  * 99999888887777766666555554444433333222221111100000
  */
-for(int i = 0; i < 5; i++) {
-    for(int j = 9; j >= 0; j--) {
-        for(int k = 0; k < 5; k++)
-            System.out.print(j);
-    }
+for(int i = 0; i < 5; i++) {
+    for(int j = 9; j >= 0; j--) {
+        for(int k = 0; k < 5; k++)
+            System.out.print(j);
+    }
     System.out.println();
 }
--- chapter2/nestedNumbers3.java
+++ chapter2/nestedNumbers3.java
@@ -5,10 +5,10 @@
  * 999999999888888887777777666666555554444333221
  * 999999999888888887777777666666555554444333221
  */
-for(int i = 0; i < 4; i++) {
-    for(int j = 9; j >= 1; j--) {
-        for(int k = 0; k < j; k++)
-            System.out.print(j);
-    }
-    System.out.println();
+for(int i = 0; i < 4; i++) {
+    for(int j = 9; j >= 1; j--) {
+        for(int k = 0; k < j; k++)
+            System.out.print(j);
+    }
+    System.out.println();
 }
