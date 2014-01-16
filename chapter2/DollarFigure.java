d1c535861d032e136c956f047bea6ea2607aae31
Alfredo Miranda <alfremir@gmail.com>
2014-01-16 10:43:25 -0600

    Completed DollarFigure

---
+++ chapter2/DollarFigure.java
@@ -0,0 +1,32 @@
+/* Write a Java program called DollarFigure that produces the following 
+ * output. Use nested for loops to capture the structure of the figure.
+ * $$$$$$$**************$$$$$$$
+ * **$$$$$$************$$$$$$**
+ * ****$$$$$**********$$$$$****
+ * ******$$$$********$$$$******
+ * ********$$$******$$$********
+ * **********$$****$$**********
+ * ************$**$************
+ */
+public class DollarFigure {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 7; i++) {
+            for(int j = 0; j < 2 * i - 2; j++)
+                System.out.print("*");
+                
+            for(int j = 0; j < -i + 8; j++)
+                System.out.print("$");
+                
+            for(int j = 0; j < -2 * i + 16; j++)
+                System.out.print("*");
+                
+            for(int j = 0; j < -i + 8; j++)
+                System.out.print("$");
+                
+            for(int j = 0; j < 2 * i - 2; j++)
+                System.out.print("*");
+                
+            System.out.println();
+        }
+    }
+}
