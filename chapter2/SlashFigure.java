ea43a2343ee6d9229b5405bfe9657e7218416413
Alfredo Miranda <alfremir@gmail.com>
2014-01-15 20:12:32 -0600

    Modified SlashFigure

--- chapter2/SlashFigure.java
+++ chapter2/SlashFigure.java
@@ -10,14 +10,14 @@
  */
 public class SlashFigure {
     public static void main(String[] args) {
-        for(int i = 22; i >= 2; i -= 4) {
-            for(int j = 0; j < (22 - i) / 2; j++)
+        for(int i = 1; i <= 6; i++) {
+            for(int j = 0; j < 2 * i - 2; j++)
                 System.out.print("\\");
                 
-            for(int j = 0; j < i; j++)
+            for(int j = 0; j < -4 * i + 26; j++)
                 System.out.print("!");
                 
-            for(int j = 0; j < (22 - i) / 2; j++)
+            for(int j = 0; j < 2 * i - 2; j++)
                 System.out.print("/");
                 
             System.out.println();
