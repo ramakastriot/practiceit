2f00a07e678ecbe7fc1c91d015bbefcaf8c9b484
Alfredo Miranda <alfremir@gmail.com>
2014-01-26 13:03:04 -0600

    Completed maxSum

--- chapter12/maxSum.java
+++ chapter12/maxSum.java
@@ -8,19 +8,19 @@
  * it executes, but L should be restored to its original state before your 
  * method returns.
  */
-public int maxSum(List<Integer> list, int n) {
-    return maxSum(list, 0, n, list.size());
+public int maxSum(List<Integer> list, int n) {
+    return maxSum(list, 0, n, list.size());
 }
 
-public int maxSum(List<Integer> list, int i, int n, int rem) {
-    if(n <= 0 || rem == 0)
-        return 0;
-    
-    int with = list.get(i) + maxSum(list, i + 1, n - list.get(i), rem - 1);
-    int withOut = maxSum(list, i + 1, n, rem - 1);
-    
-    if(with <= n && n - with < n - withOut)
-        return with;
-    
-    return withOut;
+public int maxSum(List<Integer> list, int i, int n, int rem) {
+    if(n <= 0 || rem == 0)
+        return 0;
+        
+    int with = list.get(i) + maxSum(list, i + 1, n - list.get(i), rem - 1);
+    int withOut = maxSum(list, i + 1, n, rem - 1);
+    
+    if(with <= n && n - with < n - withOut)
+        return with;
+        
+    return withOut;
 }
