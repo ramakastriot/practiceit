5401c52bda6e61ffda8210a68fbece06c18bffcd
Alfredo Miranda <alfremir@gmail.com>
2014-03-01 11:54:00 -0600

    Fixed minor formatting

--- chapter17/depthSum.java
+++ chapter17/depthSum.java
@@ -13,6 +13,6 @@ private int depthSum(IntTreeNode node, int level) {
     if(node == null)
         return 0;
         
-    return level * node.data + depthSum(node.left, level + 1) +
-        depthSum(node.right, level + 1);
+    return level * node.data +
+        depthSum(node.left, level + 1) + depthSum(node.right, level + 1);
 }
