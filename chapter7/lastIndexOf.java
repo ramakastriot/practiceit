a9d3da980c175d3973dd402b15789460c4e70b33
Alfredo Miranda <alfremir@gmail.com>
2014-04-09 22:27:58 -0500

    Fixed formatting

--- chapter7/lastIndexOf.java
+++ chapter7/lastIndexOf.java
@@ -4,11 +4,9 @@
  * found.
  */
 public int lastIndexOf(int[] arr, int val) {
-
     for(int i = arr.length - 1; i >= 0; i--) {
         if(arr[i] == val)
             return i;
     }
-    
     return -1;
 }
