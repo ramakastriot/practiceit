880110e61490dd6797295674e4323012179f7f24
Alfredo Miranda <alfremir@gmail.com>
2014-04-09 22:26:50 -0500

    Completed lastIndexOf

---
+++ chapter7/lastIndexOf.java
@@ -0,0 +1,14 @@
+/* Write a method named lastIndexOf that accepts an array of integers and an 
+ * integer value as its parameters and returns the last index at which the 
+ * value occurs in the array. The method should return -1 if the value is not 
+ * found.
+ */
+public int lastIndexOf(int[] arr, int val) {
+
+    for(int i = arr.length - 1; i >= 0; i--) {
+        if(arr[i] == val)
+            return i;
+    }
+    
+    return -1;
+}
