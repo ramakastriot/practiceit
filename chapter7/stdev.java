d39facd24b8dc4b7823e0a5bbe04d73196010491
Alfredo Miranda <alfremir@gmail.com>
2014-04-11 10:45:05 -0500

    Completed stdev

---
+++ chapter7/stdev.java
@@ -0,0 +1,23 @@
+/* Write a method called stdev that returns the standard deviation of an array 
+ * of integers. Standard deviation is computed by taking the square root of 
+ * the sum of the squares of the differences between each element and the mean,
+ * divided by one less than the number of elements. You may assume that the 
+ * array passed is non-null and contains at least two values, because the 
+ * standard deviation is undefined otherwise.
+ */
+public double stdev(int[] a) {
+    double avg = 0;
+    double num = 0;
+    
+    for(int i = 0; i < a.length; i++) {
+        avg += a[i];
+    }
+    
+    avg /= a.length;
+    
+    for(int i = 0; i < a.length; i++) {
+        num += (a[i] - avg) * (a[i] - avg);
+    }
+    
+    return Math.sqrt(num / (a.length - 1));
+}
