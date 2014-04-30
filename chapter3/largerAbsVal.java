0d3c17e484a22ac48c28f1162de699ce7e03a591
Alfredo Miranda <alfremir@gmail.com>
2014-04-29 19:42:52 -0500

    Completed largerAbsVal

---
+++ chapter3/largerAbsVal.java
@@ -0,0 +1,6 @@
+/* Write a method called largerAbsVal that takes two integers as parameters 
+ * and returns the larger of the two absolute values.
+ */
+public int largerAbsVal(int a, int b) {
+    return Math.max(Math.abs(a), Math.abs(b));
+}
