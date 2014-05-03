58d98c86a4626665e4f2d82df97e068904cdca91
Alfredo Miranda <alfremir@gmail.com>
2014-05-03 11:56:41 -0500

    Completed area

---
+++ chapter3/area.java
@@ -0,0 +1,7 @@
+/* Write a method named area that accepts the radius of a circle as a 
+ * parameter and returns the area of a circle with that radius. You may assume 
+ * that the radius is non-negative.
+ */
+public double area(double r) {
+    return Math.PI * r * r;
+}
