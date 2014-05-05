a4c33fbcac0efbc02c7554d9d07923e793899e8d
Alfredo Miranda <alfremir@gmail.com>
2014-05-04 20:04:03 -0500

    Completed scientific

---
+++ chapter3/scientific.java
@@ -0,0 +1,7 @@
+/* Write a method called scientific that accepts two real numbers as 
+ * parameters for a base and an exponent and computes the base times 10 to the 
+ * exponent, as seen in scientific notation.
+ */
+public double scientific(double base, double exp) {
+    return base * Math.pow(10, exp);
+}
