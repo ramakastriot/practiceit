5bb889c912ae0d54a66f3e2544543855cf23577c
Alfredo Miranda <alfremir@gmail.com>
2014-05-09 09:37:51 -0500

    Fixed formatting

--- chapter3/pay.java
+++ chapter3/pay.java
@@ -1,4 +1,5 @@
-/* Write a method named pay that accepts a real number for a TA's salary and an  * integer for the number of hours the TA worked this week, and returns how 
+/* Write a method named pay that accepts a real number for a TA's salary and an
+ * integer for the number of hours the TA worked this week, and returns how 
  * much money to pay the TA. The TA should receive "overtime" pay of 1 1/2 
  * normal salary for any hours above 8.
  */
