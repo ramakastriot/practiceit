bbb6a674d0fb7862192f06b2058a53f6c3e82d18
Alfredo Miranda <alfremir@gmail.com>
2014-05-09 09:25:08 -0500

    Removed weird blank characters

--- graphics/Face.java
+++ graphics/Face.java
@@ -5,23 +5,23 @@
  * 320 x 180 pixels, and the two faces' top-left corners are at (10, 30) and 
  * (150, 50).
  */
-public class Face {
-    public static void main(String[] args) {
-        DrawingPanel panel = new DrawingPanel(320, 180);
-        Graphics g = panel.getGraphics();
+public class Face {
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(320, 180);
+        Graphics g = panel.getGraphics();
         drawFace(g, 10, 30);
-        drawFace(g, 150, 50);       
+        drawFace(g, 150, 50);
     }
     
     public static void drawFace(Graphics g, int x, int y) {
         g.setColor(Color.BLACK);
-        g.drawOval(x, y, 100, 100);   // face outline
+        g.drawOval(x, y, 100, 100);   // face outline
         
         g.setColor(Color.BLUE);
-        g.fillOval(x + 20, y + 30, 20, 20);     // eyes
-        g.fillOval(x + 60, y + 30, 20, 20);
+        g.fillOval(x + 20, y + 30, 20, 20);     // eyes
+        g.fillOval(x + 60, y + 30, 20, 20);
         
         g.setColor(Color.RED);          // mouth
-        g.drawLine(x + 30, y + 70, x + 70, y + 70);
+        g.drawLine(x + 30, y + 70, x + 70, y + 70);
     }
 }
--- graphics/MickeyBox2.java
+++ graphics/MickeyBox2.java
@@ -6,10 +6,10 @@
  * use your drawFigure method to place two figures on it. One figure should be 
  * at position (50, 25) and the other should be at position (250, 45).
  */
-public class MickeyBox2 {
-    public static void main(String[] args) {
-        DrawingPanel panel = new DrawingPanel(450, 150);
-        Graphics g = panel.getGraphics();
+public class MickeyBox2 {
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(450, 150);
+        Graphics g = panel.getGraphics();
         g.setColor(Color.YELLOW);
         g.fillRect(0, 0, 450, 150);
         drawFigure(g, 50, 25);
@@ -18,11 +18,11 @@ public class MickeyBox2 {
     
     public static void drawFigure(Graphics g, int x, int y) {
         g.setColor(Color.BLUE);
-        g.fillOval(x, y, 40, 40);
-        g.fillOval(x + 80, y, 40, 40);
+        g.fillOval(x, y, 40, 40);
+        g.fillOval(x + 80, y, 40, 40);
         g.setColor(Color.RED);
-        g.fillRect(x + 20, y + 20, 80, 80);
+        g.fillRect(x + 20, y + 20, 80, 80);
         g.setColor(Color.BLACK);
-        g.drawLine(x + 20, y + 60, x + 100, y + 60);        
+        g.drawLine(x + 20, y + 60, x + 100, y + 60);        
     }
 }
