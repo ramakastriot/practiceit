e4a8322d70a9e5c45b745cf47b4afe3a882fe440
Alfredo Miranda <alfremir@gmail.com>
2014-05-13 15:08:02 -0500

    Removed weird white space

--- graphics/ShowDesign.java
+++ graphics/ShowDesign.java
@@ -7,8 +7,8 @@
  */
 public class ShowDesign {
     public static void main(String[] args) {
-        DrawingPanel panel = new DrawingPanel(200, 200);
-        Graphics g = panel.getGraphics();
+        DrawingPanel panel = new DrawingPanel(200, 200);
+        Graphics g = panel.getGraphics();
         for(int i = 0; i < 4; i++)
             g.drawRect(20 + 20 * i, 20 + 20 * i, 160 - 40 * i, 160 - 40 * i);
     }
