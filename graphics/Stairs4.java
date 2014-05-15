7976d75454a46e1d78b31278277106dadf3a3c21
Alfredo Miranda <alfremir@gmail.com>
2014-05-15 09:48:09 -0500

    Completed Stairs4

---
+++ graphics/Stairs4.java
@@ -0,0 +1,12 @@
+/* Modify your Stairs program from the previous exercise to make a new class 
+ * Stairs4 that draws the output shown below. Modify only the body of your loop.
+ */
+public class Stairs4 {
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(110, 110);
+        Graphics g = panel.getGraphics();
+        for(int i = 0; i < 10; i++) {
+            g.drawRect(5 + 10 * i, 5 + 10 * i, 100 - 10 * i, 10);
+        }
+    }
+}
