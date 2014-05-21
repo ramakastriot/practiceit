e2d268e79ddeadbbfb0619fd666e0e3634517994
Alfredo Miranda <alfremir@gmail.com>
2014-05-21 09:03:27 -0500

    Completed average

---
+++ chapter4/average.java
@@ -0,0 +1,6 @@
+/* Write a method called average that takes two integers as parameters and 
+ * returns the average of the two integers.
+ */
+public double average(int a, int b) {
+    return (a + b) / 2.0;
+}
