b5c71789b8af36eaf17c58e63f96107c18afe884
Alfredo Miranda <alfremir@gmail.com>
2014-05-25 10:36:40 -0500

    Completed flipPoint

---
+++ chapter8/flipPoint.java
@@ -0,0 +1,10 @@
+/* Add the following method to the Point class:
+ * public void flip()
+ *
+ * Negates and swaps the x/y coordinates of the Point object.
+ */
+public void flip() {
+    int temp = this.x;
+    this.x = -this.y;
+    this.y = -temp;
+}
