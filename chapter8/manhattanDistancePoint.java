9bbde620b777773f2c01324bd1c5f08637f4e851
Alfredo Miranda <alfremir@gmail.com>
2014-05-26 14:24:28 -0500

    Fixed formatting

--- chapter8/manhattanDistancePoint.java
+++ chapter8/manhattanDistancePoint.java
@@ -4,7 +4,8 @@
  *
  * Returns the "Manhattan distance" between the current Point object and the 
  * given other Point object. The Manhattan distance refers to how far apart two
- * places are if the person can only travel straight horizontally or vertically, * as though driving on the streets of Manhattan. In our case, the Manhattan 
+ * places are if the person can only travel straight horizontally or vertically,
+ * as though driving on the streets of Manhattan. In our case, the Manhattan 
  * distance is the sum of the absolute values of the differences in their 
  * coordinates; in other words, the difference in x plus the difference in y 
  * between the points.
