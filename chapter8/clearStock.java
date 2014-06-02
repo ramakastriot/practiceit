4446e666cfd0f4f02d5b5478e5294e4e5bf274ae
Alfredo Miranda <alfremir@gmail.com>
2014-06-01 20:10:19 -0500

    Completed clearStock

---
+++ chapter8/clearStock.java
@@ -0,0 +1,10 @@
+/* Add the following mutator method to the Stock class:
+ *
+ * public void clear()
+ *
+ * Resets this Stock's number of shares purchased and total cost to 0.
+ */
+public void clear() {
+    this.totalShares = 0;
+    this.totalCost = 0;
+}
