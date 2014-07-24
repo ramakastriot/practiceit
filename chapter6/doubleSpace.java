5858d8ed21b036887917570f08b319af4cdd3bc5
Alfredo Miranda <alfremir@gmail.com>
2014-07-24 16:53:08 -0500

    Fix typo

--- chapter6/doubleSpace.java
+++ chapter6/doubleSpace.java
@@ -1,5 +1,5 @@
 /* Write a method named doubleSpace that accepts a Scanner for an input file 
- * and a PrintStream for an output file as its parameters, writing into the ]
+ * and a PrintStream for an output file as its parameters, writing into the
  * output file a double-spaced version of the text in the input file.
  */
 public void doubleSpace(Scanner sc, PrintStream ps) {
