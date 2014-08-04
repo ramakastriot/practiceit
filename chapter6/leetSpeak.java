604847bf534f80c7c19920dbb9e149cb34580ec9
Alfredo Miranda <alfremir@gmail.com>
2014-08-04 17:53:42 -0500

    Coded it in another way

--- chapter6/leetSpeak.java
+++ chapter6/leetSpeak.java
@@ -17,25 +17,41 @@
 public void leetSpeak(Scanner input, PrintStream output) {
     while(input.hasNextLine()) {
         String line = input.nextLine();
+        StringBuilder leetLine = new StringBuilder();
         
-        line = line.replace('o', '0');
-        line = line.replace('l', '1');
-        line = line.replace('e', '3');
-        line = line.replace('a', '4');
-        line = line.replace('t', '7');
-        
-        Scanner lineSc = new Scanner(line);
-        
-        while(lineSc.hasNext()) {
-            String token = lineSc.next();
+        for(int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
             
-            if(token.charAt(token.length() - 1) == 's')
-                token = token.substring(0, token.length() - 1) + 'Z';
-            
-            output.print("(" + token + ") ");
+            if(Character.isWhitespace(c)) {
+                leetLine.append(c);
+            } else {
+                if(i == 0 || 
+                    (i > 0 && Character.isWhitespace(line.charAt(i - 1))) )
+                    leetLine.append('(');
+                
+                if(c == 'o') {
+                    leetLine.append('0');
+                } else if(c == 'l') {
+                    leetLine.append('1');
+                } else if(c == 'e') {
+                    leetLine.append('3');
+                } else if(c == 'a') {
+                    leetLine.append('4');
+                } else if(c == 't') {
+                    leetLine.append('7');
+                } else if(c == 's' && (i == line.length() - 1 || 
+                    Character.isWhitespace(line.charAt(i + 1)))) {
+                    leetLine.append('Z');
+                } else {
+                    leetLine.append(c);
+                }
+                
+                if(i == line.length() - 1 || 
+                    Character.isWhitespace(line.charAt(i + 1)))
+                    leetLine.append(')');
+            }
         }
         
-        output.println();
-                     
+        output.println(leetLine);
     }
 }
