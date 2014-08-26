c0b784363b7d962348a47addf0da8fb353af0042
Alfredo Miranda <alfremir@gmail.com>
2014-08-25 23:44:06 -0500

    Completed MinMaxAccount

---
+++ chapter9/MinMaxAccount.java
@@ -0,0 +1,42 @@
+public class MinMaxAccount extends BankingAccount {
+    private int minBalance;
+    private int maxBalance;
+    
+    public MinMaxAccount(Startup s) {
+        super(s);
+        minBalance = s.startup_getBalance();
+        maxBalance = s.startup_getBalance();
+    }
+    
+    public void debit(Debit d) {
+        super.debit(d);
+        
+        int balance = getBalance();
+        
+        if(balance < minBalance)
+            minBalance = balance;
+        
+        if(balance > maxBalance)
+            maxBalance = balance;
+    }
+    
+    public void credit(Credit c) {
+        super.credit(c);
+        
+        int balance = getBalance();
+        
+        if(balance < minBalance)
+            minBalance = balance;
+        
+        if(balance > maxBalance)
+            maxBalance = balance;
+    }
+    
+    public int getMin() {
+        return minBalance;
+    }
+    
+    public int getMax() {
+        return maxBalance;
+    }
+}
