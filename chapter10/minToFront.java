9d9b05a41704a79a8f30ba9f402f1693bd07a0d8
Alfredo Miranda <alfremir@gmail.com>
2014-11-16 00:01:21 -0600

    Use size instead of length

--- chapter10/minToFront.java
+++ chapter10/minToFront.java
@@ -1,8 +1,14 @@
+/* Write a method minToFront that takes an ArrayList of integers as a 
+ * parameter and that moves the minimum value in the list to the front, 
+ * otherwise preserving the order of the elements. You may assume that the 
+ * list stores at least one value.
+ */
+
 public void minToFront(ArrayList<Integer> arr) {
     int min = Integer.MAX_VALUE;
     int pos = -1;
     
-    for(int i = 0; i < arr.length(); i++) {
+    for(int i = 0; i < arr.size(); i++) {
         if(arr.get(i) < min) {
             min = arr.get(i);
             pos = i;
@@ -10,5 +16,5 @@ public void minToFront(ArrayList<Integer> arr) {
     }
     
     arr.remove(pos);
-    arr.add(min);
+    arr.add(0, min);
 }
