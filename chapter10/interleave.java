2eddced64aab409237f47886648ebfe337a0e878
Alfredo Miranda <alfremir@gmail.com>
2014-11-16 23:43:01 -0600

    Use a boolean instead of int to determine which element to add to ArrayList

--- chapter10/interleave.java
+++ chapter10/interleave.java
@@ -8,18 +8,18 @@ public void interleave(ArrayList<Integer> a1, ArrayList<Integer> a2) {
     int size2 = a2.size();
     int index1 = 0;
     int index2 = 0;
-    int n = 0;
-
+    boolean flag = true;
+    
     while(index1 < size1 || index2 < size2) {
-        if(index1 < size1 && n % 2 == 0) {
+        if(index1 < size1 && flag) {
             a1.add(a1.get(index1));
             index1++;
-        } else if(index2 < size2 && n % 2 == 1) {
+        } else if(index2 < size2 && !flag) {
             a1.add(a2.get(index2));
             index2++;
         }
-        n++;
+        flag = !flag;
     }
-
+    
     a1.subList(0, size1).clear();
 }
