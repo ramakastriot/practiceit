216c2e4e451b2d64708ffa90dd8e1df3d6801d4c
Sid <sidharth.m.lakshmanan@gmail.com>
2019-12-01 21:04:22 -0800

    Much Simpler

--- chapter16/split.java
+++ chapter16/split.java
@@ -2,34 +2,17 @@
  * the negative values appear before all of the non-negatives.
  */
 public void split() {
-    if(front == null)
-        return;
-        
-    ListNode current = front;
-    ListNode neg = null;
-    ListNode pos = null;
-    ListNode negLast = null;
-    
-    while(current != null) {
-        ListNode next = current.next;
-        
-        if(current.data < 0) {
-            current.next = neg;
-            neg = current;
-            if(neg.next == null)
-                negLast = neg;
-        } else {
-            current.next = pos;
-            pos = current;
+    ListNode curr = front;
+    while (curr != null && curr.next != null) {
+        if (curr.next.data < 0) {
+            ListNode temp = curr.next.next;
+            ListNode neg = curr.next;
+            neg.next = front;
+            front = neg;
+            curr.next = temp;
         }
-        
-        current = next;
-    }
-    
-    if(neg == null) {
-        front = pos;
-    } else {
-        negLast.next = pos;
-        front = neg;
+        else {
+            curr = curr.next;
+        }
     }
 }
